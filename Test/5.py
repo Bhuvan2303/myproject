cb6f850cc1825f8d6394f8e7a021e94eb4e2c8fb
Bhuvan H R <101463687+Bhuvan2303@users.noreply.github.com>
2025-11-28 06:27:53 +0530

    Delete Test directory

--- Test/5.py
+++
@@ -1,9 +0,0 @@
-class Secret:
-    def __init__(self, password):
-        self.__password = password   # private
-
-    def get_password(self):
-        print("Password:", self.__password)
-
-c = Secret(4376)
-c.get_password()
